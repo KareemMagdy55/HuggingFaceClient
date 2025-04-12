f631064dc9174190ae0b214c41cccb6217a445e7
Kareem Magdy <kareemmagdy1175@gmail.com>
2025-04-12 22:45:07 +0200

    Add main services classes

---
+++ HuggingFaceHub/HuggingFaceHub/Service/ChatCompletionService.cs
@@ -0,0 +1,10 @@
+﻿using HuggingFaceHub.DTO.ChatCompletion;
+using HuggingFaceHub.DTO.ChatCompletion.Response;
+
+namespace HuggingFaceHub.Service;
+
+internal class ChatCompletionService {
+    public ChatCompletionResponseDto Create(ChatCompletionRequestDto chatCompletionRequestDto) {
+        
+    }
+}
---
+++ HuggingFaceHub/HuggingFaceHub/Service/HuggingFaceClientService.cs
@@ -0,0 +1,8 @@
+﻿using HuggingFaceHub.DTO.ChatCompletion;
+using HuggingFaceHub.DTO.ChatCompletion.Response;
+
+namespace HuggingFaceHub.Service;
+
+public class HuggingFaceClientService {
+
+}
